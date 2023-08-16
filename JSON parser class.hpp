08d0faa892fb06ac6b475fdd09d81a37216bc046
egor <agapeev.egor@yandex.ru>
2023-08-16 20:15:33 +0300

    added path with examples and simple example for parsing, in main.cpp tested parsing example 1, some info about parser added in README

--- JSON parser class.hpp
+++ JSON parser class.hpp
@@ -7,5 +7,21 @@ using string = std::string;
 
 struct parser{
 
+    private:
+    static void identify_command(string command_name){
+
+    }
+    static void make_variable(){
+
+    }
+    static void give_value_to_variable(){
+
+    }
+    static void make_mark(){
+
+    }
+    static void follow_mark_if(){
+
+    }
 };
 #endif //JSON_PARSER_JSON_PARSER_CLASS_H
--- main.cpp
+++ main.cpp
@@ -3,7 +3,25 @@
 #include <fstream>
 #include "json.hpp"
 
+using ifstream = std::ifstream;
+using json = nlohmann::json;
+using string = std::string;
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ifstream file("../examples of json files/example 1.json");
+    if(file.is_open()){
+        json data = json::parse(file);
+        string pr = data[0];
+        string s[] = {to_string(data[1][0]), data[1][1]};
+        string es = data[2];
+
+// Print the values
+        std::cout << "Who: " << pr << std::endl;
+        std::cout << "Variable: " << s[0] + " " + s[1] << std::endl;
+        std::cout << "How: " << es << std::endl;
+    } else{
+        std::cout << "file was not opened";
+    }
     return 0;
 }
