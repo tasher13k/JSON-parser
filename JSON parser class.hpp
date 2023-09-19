f66578f8663cce8cc2a862de7658922dfa9eb844
egor <agapeev.egor@yandex.ru>
2023-09-19 03:55:12 +0300

    style: format code

--- JSON parser class.hpp
+++ JSON parser class.hpp
@@ -1,5 +1,6 @@
 #ifndef JSON_PARSER_JSON_PARSER_CLASS_H
 #define JSON_PARSER_JSON_PARSER_CLASS_H
+
 #include <iostream>
 #include <unordered_map>
 #include "json.hpp"
@@ -9,25 +10,30 @@ using json = nlohmann::json;
 using string = std::string;
 using iter_on_const_json = json::const_iterator;
 using ref_on_iter_on_const_json = iter_on_const_json &;
-using pointer_to_methods_for_commands = iter_on_const_json (* const)(ref_on_iter_on_const_json);
+using pointer_to_methods_for_commands = iter_on_const_json (*const)(ref_on_iter_on_const_json);
 //this type will be necessary to call functions depending on the name from the parsed file
 //this return type helps with "follow mark" command via value from map_for_marks which stores iterator on command in parsed file
 
 using map_type_for_commands = const std::unordered_map<string, pointer_to_methods_for_commands>;
 using map_type_for_marks = std::unordered_map<string, json::const_iterator>;
 
-struct parser{
+struct parser {
     static void get_all_values_and_marks();
 
-    static void iterating_over_commands(json const & list_of_commands);
+    static void iterating_over_commands(json const &list_of_commands);
+
 private:
     static iter_on_const_json make_variable(ref_on_iter_on_const_json);
+
     static iter_on_const_json give_value_to_variable(ref_on_iter_on_const_json);
+
     static iter_on_const_json make_mark(ref_on_iter_on_const_json);
+
     static iter_on_const_json follow_mark_if(ref_on_iter_on_const_json);
 
     static map_type_for_commands map_for_commands;
     static cparse::TokenMap map_for_vars;
     static map_type_for_marks map_for_marks;
 };
+
 #endif //JSON_PARSER_JSON_PARSER_CLASS_H
--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ using string = std::string;
 
 int main() {
     ifstream file("../examples of json files/example 4.json");
-    if(file.is_open()){
+    if (file.is_open()) {
         json data = json::parse(file);
         parser::iterating_over_commands(data);
         parser::get_all_values_and_marks();
-    } else{
+    } else {
         std::cout << "file was not opened";
     }
     return 0;
